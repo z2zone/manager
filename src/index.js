f54964943987cb086815bbb9e148a42bc8352f42
Brandon Cho <hjun1114@gmail.com>
2023-03-12 23:00:47 -0400

    adding template

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Grab the root component
+const root = document.getElementById("root");
+//Create react root
+const ReactRoot = ReactDOM.createRoot(root);
+//Render it
+ReactRoot.render(<App />);
